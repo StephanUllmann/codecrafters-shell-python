845576771c8e501052bbaab9ae883bf017db8784
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-01-05 13:53:55 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,8 @@ import sys
 
 def main():
     while True:
-        sys.stdout.write("$ ")
-        cmd = ""
-        for line in sys.stdin:
-            cmd += line.rstrip()
-            break
-
-        sys.stdout.write(f"{cmd}: command not found")
+        cmd = input("$ ")
+        sys.stdout.write(f"{cmd}: command not found\n")
 
 
 if __name__ == "__main__":
