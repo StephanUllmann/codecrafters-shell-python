67b2bf564938338a4b34994e68e161dd2e3382d0
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-01-05 16:56:16 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import sys
 
 
+def echo(message):
+    print(*message)
+
+
 def main():
     while True:
-        cmd = input("$ ")
-        if "exit" == cmd.lower():
-            break
-        sys.stdout.write(f"{cmd}: command not found\n")
+        user_input = input("$ ").split()
+
+        cmd = user_input[0].lower()
+        args = user_input[1:]
+
+        match cmd:
+            case "exit":
+                break
+            case "echo":
+                echo(args)
+            case _:
+                sys.stdout.write(f"{cmd}: command not found\n")
 
 
 if __name__ == "__main__":
